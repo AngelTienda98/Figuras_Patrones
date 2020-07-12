3a4d3de406743f898a0d96b5c04ac5473572bba3
tiend <tiend@LAPTOP-MBBCHR34>
2020-07-12 15:40:20 -0500

    Estacionamiento version 0.02

---
+++ src/formularios/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formularios;
+
+/**
+ *
+ * @author tiend
+ */
+public class Main {
+    public static void main(String[] args){
+        Menu mu =  new Menu();
+        mu.setVisible(true);
+    }
+    
+}
